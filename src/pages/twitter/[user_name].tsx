d562eb93dfcc9b8e0d69236e6cdac1776ea67517
komekami <komekami0131@gmail.com>
2022-07-09 19:38:31 +0900

    feat: TwitterCardの形で取得ツイートを表示する処理を実装

--- src/pages/twitter/[user_name].tsx
+++ src/pages/twitter/[user_name].tsx
@@ -2,19 +2,28 @@ import { Box } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import React from "react";
-import useSWR, { Fetcher } from "swr";
+import useSWR from "swr";
+import { TwitterTweetEmbed } from "react-twitter-embed";
 
 const TwitterUserName: NextPage = () => {
   const router = useRouter()
-  const name = router.query.user_name
+  const name = router.query.user_name ?? ""
 
   const fetcher = (url: string): Promise<any> => fetch(url).then(res => res.json())
-  const { data } = useSWR(`/api/tweets?name=${name}`, fetcher)
+  const { data, error } = useSWR(`/api/tweets?name=${name}`, fetcher)
 
   return (
     <>
       <Box>{name}</Box>
-      <Box>{JSON.stringify(data, null, 2)}</Box>
+      {/* useEffectはレンダリングされたタイミングで実行されるので初期値がnullだとエラー */}
+      {data?.tweets.map((value :any, key :number) => {
+        {console.log(value, key)}
+        return (
+          <Box>
+            <TwitterTweetEmbed tweetId={`${value.id}`}/>
+          </Box>
+        )
+      })}
     </>
   )
 }
