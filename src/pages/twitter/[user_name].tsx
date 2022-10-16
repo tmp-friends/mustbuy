a4e01666d445da40b385e61f5888c3de200c17a0
komekami <komekami0131@gmail.com>
2022-10-16 18:56:56 +0900

    refactor: 取得データが空配列の場合の処理を実装

--- src/pages/twitter/[user_name].tsx
+++ src/pages/twitter/[user_name].tsx
@@ -1,4 +1,4 @@
-import { Text } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import React from "react";
@@ -6,27 +6,32 @@ import useSWR from "swr";
 import { TwitterTweetEmbed } from "react-twitter-embed";
 
 const TwitterUserName: NextPage = () => {
-  const router = useRouter()
-  const name = router.query.user_name ?? ""
+  const router = useRouter();
+  const name = router.query.user_name ?? "";
 
-  const fetcher = (url: string): Promise<any> => fetch(url).then(res => res.json())
-  const { data, error } = useSWR(`/api/tweets?name=${name}`, fetcher)
+  const fetcher = (url: string): Promise<any> =>
+    fetch(url).then((res) => res.json());
+  const { data, error } = useSWR(`/api/tweets?name=${name}`, fetcher);
+
+  if (error) return <Box>Failed to load</Box>;
+  if (!data) return <Box>Now Loading...</Box>;
 
   return (
     <>
-      <Text fontSize="md">@{name}</Text>
-      <Text fontSize="xs" opacity="0.7" pb="4">
-        検索結果を表示するまでに数秒かかることがあります
+      <Text fontSize="md" pb={4}>
+        @{name} さんの いいねしたツイート
       </Text>
       {/* useEffectはレンダリングされたタイミングで実行されるので初期値がnullだとエラー */}
       {/* TODO: dataに型付けする */}
-      {data?.tweets.map((value: any, index: number) => {
-        return (
-          <TwitterTweetEmbed key={index} tweetId={`${value.id}`}/>
-        )
-      })}
+      {data.tweets.length ? (
+        data.tweets.map((value: any, index: number) => {
+          return <TwitterTweetEmbed key={index} tweetId={`${value.id}`} />;
+        })
+      ) : (
+        <Box>LikedTweets not found</Box>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default TwitterUserName
+export default TwitterUserName;
