20031740786d6511807915e766b6bf8bab6318ab
komekami <komekami0131@gmail.com>
2022-10-17 05:51:02 +0900

    refactor: ヘッダーにcontent-wrapperを使わないよう改修

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -1,19 +1,17 @@
-import { FC } from "react"
-import Link from "next/link"
-import { Flex, Text } from "@chakra-ui/react"
+import { FC } from "react";
+import Link from "next/link";
+import { Flex, Text } from "@chakra-ui/react";
 
-import { ContentWrapper } from "./ContentWrapper"
+import { ContentWrapper } from "./ContentWrapper";
 
 export const SiteHeader: FC = () => {
   return (
-    <Flex as="header" shadow="md">
-      <ContentWrapper>
-        <Link href="/" passHref>
-          <Text fontSize="xl" fontWeight="semibold">
-            MustBuy
-          </Text>
-        </Link>
-      </ContentWrapper>
+    <Flex as="header" shadow="md" p={3}>
+      <Link href="/" passHref>
+        <Text fontSize="xl" fontWeight="semibold">
+          MustBuy
+        </Text>
+      </Link>
     </Flex>
-  )
-}
+  );
+};
